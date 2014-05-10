45ebed52453709d6f1c09e3264e779aad4bdb00d
Roger Campos <roger@itnig.net>
2014-05-10 15:14:24 +0200

    Add movement

--- main.rb
+++ main.rb
@@ -4,27 +4,51 @@ Dir[File.join(File.dirname(__FILE__), "models/**")].each {|x| require x}
 
 class GameWindow < Gosu::Window
   def initialize
-    @screen_width = 800
+    @screen_width = 1000
     @screen_height = 600
     super @screen_width, @screen_height, false
     self.caption = "Gosu Tutorial Game"
 
     @cube = Cube.new 1, Point.new(10, 0, 10)
     @camera = Camera.new Point.new(10, 0, 0), Vector.new(0, 0, 1)
+
+    @velocity = 0.1
   end
 
   def update
+    if button_down? Gosu::KbLeft
+      @cube.position.x -= @velocity
+    end
+
+    if button_down? Gosu::KbRight
+      @cube.position.x += @velocity
+    end
+
+    if button_down? Gosu::KbUp
+      @cube.position.y += @velocity
+    end
+
+    if button_down? Gosu::KbDown
+      @cube.position.y -= @velocity
+    end
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
   end
 
   def draw
     @cube.world_points.each do |point|
       x, y = calculate_object_screen_coords(point)
 
-      x = x + @screen_width / 2
-      y = -y + @screen_height / 2
+      # Y dimension goes up in 3d-world by down in screen-world
+      y = -y
 
-      x = x.to_i
-      y = y.to_i
+      # Position the camera at the center of the screen
+      x = x + @screen_width / 2
+      y = y + @screen_height / 2
 
       draw_triangle x-5, y-5, Gosu::Color::WHITE, x, y, Gosu::Color::WHITE, x, y+5, Gosu::Color::WHITE
     end
